bff05e138e17b86433e9538b910cde3a59dd5233
Ben Forsrup <ben@newstag.com>
2019-06-02 18:47:08 +0200

    bunch of projects work

--- src/templates/work.js
+++ src/templates/work.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Slider from "react-slick";
 import { HelmetDatoCms } from "gatsby-source-datocms";
 import Img from "gatsby-image";
@@ -7,27 +7,59 @@ import Layout from "../components/layout";
 import classNames from "classnames";
 import * as _ from "lodash";
 import { Parallax, Background } from "react-parallax";
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css"; // This only needs to be imported once in your app
 
 export default ({ data }) => {
-  console.log(data);
+  const [photoIndex, setPhotoIndex] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+  let sheetStyle = classNames({
+    'sheet': true,
+    [data.datoCmsWork.shorthand]: true,
+  })
+
   let headerContainer = classNames({
     sheet__headerContainer: true,
     [data.datoCmsWork.shorthand]: true,
     background: !_.isEmpty(data.datoCmsWork.background)
   });
 
+  let headerTitle = classNames({
+    sheet__title: true,
+    sheet__customHeader:
+      data.datoCmsWork.shorthand === "newstag" ||
+      data.datoCmsWork.shorthand === "riksdagsviz" ||
+      data.datoCmsWork.shorthand === "kex" ||
+      data.datoCmsWork.shorthand === "vizstudent" ||
+      data.datoCmsWork.shorthand === "vizgapm" ||
+      data.datoCmsWork.shorthand === "lightsphere"||
+      data.datoCmsWork.shorthand === "jengar" ||
+      data.datoCmsWork.shorthand === "badplatser"
+  });
+
+
+
   let imageStyle = {
     backgroundImage:
       data.datoCmsWork.background &&
       "url(" + data.datoCmsWork.background.url + ")"
   };
+
+  const images = data.datoCmsWork.gallery.map(({ fluid }) => fluid.src);
+
+  const isMobileApp = !_.isEmpty(data.datoCmsWork.mobileTemplate);
+
+  const hasMobileContent = !_.isEmpty(data.datoCmsWork.mobileContent)
+
+  console.log(isMobileApp);
   return (
     <Layout>
-      <article className="sheet">
+      <article className={sheetStyle}>
         <HelmetDatoCms seo={data.datoCmsWork.seoMetaTags} />
         <div className="sheet__inner">
           <Parallax
-            blur={15}
+            blur={25}
             bgImage={
               data.datoCmsWork.background
                 ? data.datoCmsWork.background.url
@@ -38,11 +70,32 @@ export default ({ data }) => {
             strength={300}
           >
             <div className={headerContainer}>
-              <h1 className="sheet__title">
-                <a href={data.datoCmsWork.urllink && data.datoCmsWork.urllink}>
-                  {data.datoCmsWork.title}
-                </a>
-              </h1>
+              {isMobileApp ? (
+                <div className="mobile__container"> 
+                <div className="mobile__content_wrapper">
+                  <img 
+                    className="mobile__template"
+                    alt={data.datoCmsWork.title}
+                    src={data.datoCmsWork.mobileTemplate.url}
+                  />  
+                  </div>
+                  <h1 className={headerTitle}>
+                  <a
+                    href={data.datoCmsWork.urllink && data.datoCmsWork.urllink}>
+                      {data.datoCmsWork.title}
+                    </a>
+                  </h1>
+                  
+                </div>
+              ) : (
+                <h1 className={headerTitle}>
+                  <a
+                    href={data.datoCmsWork.urllink && data.datoCmsWork.urllink}
+                  >
+                    {data.datoCmsWork.title}
+                  </a>
+                </h1>
+              )}
             </div>
           </Parallax>
 
@@ -51,15 +104,37 @@ export default ({ data }) => {
               <p className="sheet__lead">{data.datoCmsWork.excerpt}</p>
               <div className="sheet__slider">
                 <Slider infinite={true} slidesToShow={2} arrows>
-                  {data.datoCmsWork.gallery.map(({ fluid }) => (
+                  {images.map((src, index) => (
                     <img
+                      onClick={() => {
+                        setPhotoIndex(index);
+                        setIsOpen(true);
+                      }}
                       alt={data.datoCmsWork.title}
-                      key={fluid.src}
-                      src={fluid.src}
+                      key={src}
+                      src={src}
                     />
                   ))}
                 </Slider>
               </div>
+              {isOpen && (
+                <Lightbox
+                  mainSrc={images[photoIndex]}
+                  nextSrc={images[(photoIndex + 1) % images.length]}
+                  prevSrc={
+                    images[(photoIndex + images.length - 1) % images.length]
+                  }
+                  onCloseRequest={() => setIsOpen(false)}
+                  onMovePrevRequest={() =>
+                    setPhotoIndex(
+                      (photoIndex + images.length - 1) % images.length
+                    )
+                  }
+                  onMoveNextRequest={() =>
+                    setPhotoIndex((photoIndex + 1) % images.length)
+                  }
+                />
+              )}
               <div
                 className="sheet__body"
                 dangerouslySetInnerHTML={{
@@ -67,9 +142,6 @@ export default ({ data }) => {
                     data.datoCmsWork.descriptionNode.childMarkdownRemark.html
                 }}
               />
-              <div className="sheet__gallery">
-                <Img fluid={data.datoCmsWork.coverImage.fluid} />
-              </div>
             </div>
           </div>
         </div>
@@ -105,6 +177,18 @@ export const query = graphql`
         }
       }
       coverImage {
+        url
+        fluid(maxWidth: 600, imgixParams: { fm: "jpg", auto: "compress" }) {
+          ...GatsbyDatoCmsSizes
+        }
+      }
+      mobileTemplate {
+        url
+        fluid(maxWidth: 600, imgixParams: { fm: "jpg", auto: "compress" }) {
+          ...GatsbyDatoCmsSizes
+        }
+      }
+      mobileContent {
         url
         fluid(maxWidth: 600, imgixParams: { fm: "jpg", auto: "compress" }) {
           ...GatsbyDatoCmsSizes
